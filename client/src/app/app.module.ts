f03d71a86736d4506a29a1cde0eab5bfd6ed66cc
jisaagc <anne.trent@jbhunt.com>
2022-09-22 14:38:37 -0600

    added modal

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BadgeModule } from 'primeng-lts/badge';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { ButtonModule } from 'primeng-lts/button';
-import {CardModule} from 'primeng-lts/card';
+import { CardModule } from 'primeng-lts/card';
 import { CommonModule } from '@angular/common';
-import {DataViewModule} from 'primeng-lts/dataview';
+import { DataViewModule } from 'primeng-lts/dataview';
+import { DialogModule } from 'primeng-lts/dialog';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ScrollingModule } from '@angular/cdk/scrolling';
@@ -15,6 +17,7 @@ import { GroupListComponent } from './group-list/group-list.component';
 import { RouterModule } from '@angular/router';
 import { StateCardComponent } from './state-card/state-card.component';
 import { StateListComponent } from './state-list/state-list.component';
+import { GroupModalComponent } from './group-modal/group-modal.component';
 
 @NgModule({
   declarations: [
@@ -23,15 +26,19 @@ import { StateListComponent } from './state-list/state-list.component';
     GroupListComponent,
     StateCardComponent,
     StateListComponent,
+    GroupModalComponent,
   ],
   imports: [
     AppRoutingModule,
     BadgeModule,
+    BrowserModule,
+    BrowserAnimationsModule,
     ButtonModule,
     BrowserModule,
     CardModule,
     CommonModule,
     DataViewModule,
+    DialogModule,
     HttpClientModule,
     RouterModule,
     ScrollingModule
--- client/src/app/group-list/group-list.component.ts
+++ client/src/app/group-list/group-list.component.ts
@@ -4,26 +4,30 @@ import { GroupsService } from '../services/groups.service';
 @Component({
   selector: 'app-group-list',
   templateUrl: './group-list.component.html',
-  styleUrls: ['./group-list.component.scss']
+  styleUrls: ['./group-list.component.scss'],
 })
 export class GroupListComponent implements OnInit {
   groups = [];
+  display: boolean = false;
 
-  constructor(private groupsService: GroupsService) { }
+  constructor(private groupsService: GroupsService) {}
 
   ngOnInit(): void {
     this.groupsService.getGroups().subscribe({
       next: (response) => {
         this.groups = response;
-        console.log("GROUPS: ", response);
+        console.log('GROUPS: ', response);
       },
       error: () => {
-        console.log("groups oops");
+        console.log('groups oops');
       },
       complete: () => {
-        console.log("groups done");
-      }
-    })
+        console.log('groups done');
+      },
+    });
   }
 
+  showDialog() {
+    this.display = true;
+  }
 }
