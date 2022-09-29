db4b906ab6480e02b3bf97e3663c900f58161908
jisaagc <anne.trent@jbhunt.com>
2022-09-29 13:26:53 -0600

    member modal validationworking

--- client/src/app/member-modal/member-modal.component.ts
+++ client/src/app/member-modal/member-modal.component.ts
@@ -30,7 +30,8 @@ export class MemberModalComponent implements OnInit {
       this.memberForm = new FormGroup(
         {
           MemberName: new FormControl(this.selectedMember.MemberName, [
-            Validators.required, Validators.minLength(4)
+            Validators.required,
+            Validators.minLength(4),
           ]),
           MemberPhone: new FormControl(this.selectedMember.MemberPhone, [
             Validators.required,
@@ -65,24 +66,26 @@ export class MemberModalComponent implements OnInit {
   }
 
   onSubmit(formValues: any): void {
-    this.groupService.addMember(formValues, this.groupId).subscribe(
-      () => {
-        this.closeModal();
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Confirmed',
-          detail: `${formValues.MemberName} was successfully added!`,
-        });
-        this.router.navigate([`members/${this.groupId}`]);
-      },
-      () => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: `Something went wrong, ${formValues.MemberName} was not able to be added at this time`,
-        });
-      }
-    );
+    if (this.isValid()) {
+      this.groupService.addMember(formValues, this.groupId).subscribe(
+        () => {
+          this.closeModal();
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Confirmed',
+            detail: `${formValues.MemberName} was successfully added!`,
+          });
+          this.router.navigate([`members/${this.groupId}`]);
+        },
+        () => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: `Something went wrong, ${formValues.MemberName} was not able to be added at this time`,
+          });
+        }
+      );
+    }
   }
 
   // onSubmit(formValues: any): void {
@@ -97,28 +100,39 @@ export class MemberModalComponent implements OnInit {
   // }
 
   onUpdate(formValues: any): void {
-    console.log('Edit Member', formValues, this.groupId);
-    this.groupService.editMember(formValues, this.groupId).subscribe(
-      (response: any) => {
-        this.closeModal();
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Confirmed',
-          detail: `Changes to ${formValues.MemberName} were saved.`,
-        });
-      },
-      () => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: `Something went wrong, changes for ${formValues.MemberName} were not able to be saved at this time`,
-        });
-      }
-    );
+    if (this.isValid()) {
+      this.groupService.editMember(formValues, this.groupId).subscribe(
+        (response: any) => {
+          this.closeModal();
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Confirmed',
+            detail: `Changes to ${formValues.MemberName} were saved.`,
+          });
+        },
+        () => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: `Something went wrong, changes for ${formValues.MemberName} were not able to be saved at this time`,
+          });
+        }
+      );
+    }
   }
 
   closeModal() {
     this.close.emit(true);
+  }
+
+  isValid(): boolean {
+    console.log('FORM', this.memberForm.valid);
+    if (this.memberForm.valid) {
+      return true;
+    } else {
+      this.memberForm.markAllAsTouched();
+      return false;
+    }
   }
 
   deleteMember(event: any) {
