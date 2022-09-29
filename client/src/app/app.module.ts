bf0240fadb706f42285b4be325823f97f3b795b6
jisaagc <anne.trent@jbhunt.com>
2022-09-29 08:00:17 -0600

    adding styling to member-list

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { BadgeModule } from 'primeng-lts/badge';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng-lts/button';
 import { CardModule } from 'primeng-lts/card';
 import { CommonModule } from '@angular/common';
-import {ConfirmPopupModule} from 'primeng-lts/confirmpopup';
+import { ConfirmPopupModule } from 'primeng-lts/confirmpopup';
 import { DataViewModule } from 'primeng-lts/dataview';
 import { DialogModule } from 'primeng-lts/dialog';
-import {DividerModule} from 'primeng-lts/divider';
-import {DropdownModule} from 'primeng-lts/dropdown';
+import { DividerModule } from 'primeng-lts/divider';
+import { DropdownModule } from 'primeng-lts/dropdown';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {InputNumberModule} from 'primeng-lts/inputnumber';
+import { InputNumberModule } from 'primeng-lts/inputnumber';
 import { InputTextModule } from 'primeng-lts/inputtext';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { ToastModule } from 'primeng-lts/toast';
 import { ConfirmationService } from 'primeng-lts/api';
 
 import { AppComponent } from './app.component';
@@ -59,9 +60,10 @@ import { MemberModalComponent } from './member-modal/member-modal.component';
     InputTextModule,
     ReactiveFormsModule,
     RouterModule,
-    ScrollingModule
+    ScrollingModule,
+    ToastModule,
   ],
   providers: [ConfirmationService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
